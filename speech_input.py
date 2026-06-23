740f26a95a3647dc7f3139dd594719d7e4bcca3a
shoheb mulla <149918582+shoheb07@users.noreply.github.com>
2026-06-23 23:33:44 +0530

    Create speech_input.py

---
+++ speech_input.py
@@ -0,0 +1,16 @@
+import speech_recognition as sr
+
+def listen():
+    recognizer = sr.Recognizer()
+
+    with sr.Microphone() as source:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(source)
+        audio = recognizer.listen(source)
+
+    try:
+        text = recognizer.recognize_google(audio)
+        print("You said:", text)
+        return text.lower()
+    except:
+        return ""
